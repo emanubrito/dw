2a30cfabe8709fcd2c1a8412992ce5cb6358b0ae
Emanuelle <debrito.emanu@gmail.com>
2024-11-21 22:23:33 -0300

    21/11

--- objetos/ex7.js
+++ objetos/ex7.js
@@ -1,7 +1,11 @@
-const carrinho = { itens: [{ nome: "Camisa", preco: 50 },
-{ nome: "Calça", preco: 100 }],
-calcularTotal: function(){
-    return this.itens.reduce((total, item) => total + item.preco, 0)
-}
-}
-console.log(carrinho.calcularTotal)
+const carrinho = {
+    itens: [
+      { nome: "Camisa", preco: 50 },
+      { nome: "Calça", preco: 100 }
+    ],
+    calcularTotal: function () {
+      return this.itens.reduce((total, item) => total + item.preco, 0);
+    }
+  };
+
+  console.log(carrinho.calcularTotal());
---
+++ objetos/ex8.js
@@ -0,0 +1,8 @@
+const aluno = {
+    nome: "Maria",
+    idade: 20,
+    curso: "Ciências da Computação"
+  };
+  const { nome, curso } = aluno;
+  
+  console.log(`Nome: ${nome}, Curso: ${curso}`);
